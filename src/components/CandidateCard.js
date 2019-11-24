a8953ef5a27eb5e0bbfb7cddfa11e8a77c960b91
cdnphantom <cduynguyen95@gmail.com>
2019-11-24 10:56:22 -0500

    image edit

--- src/components/CandidateCard.js
+++ src/components/CandidateCard.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import '../styles/CandidateCard.css';
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from '@material-ui/core/CardMedia';
 
-const useStyles = makeStyles({
-    card: {
-        maxWidth: 150,
-    },
-    media: {
-        height: 1000,
-    },
-});
-
 const CandidateCard = (props) => {
-    const classes = useStyles();
 
     return (
         <Card>
             <CardActionArea>
                 {props.candidate.name}
-                <CardMedia className={classes.media} image={props.candidate.image} />
+                <CardMedia className='cardImages' image={props.candidate.image} />
             </CardActionArea>
         </Card>
     );
